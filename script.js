a551d07c837e9a5c585a6d9d703a581698569951
Dan Gross <danwgross@gmail.com>
2022-11-11 09:35:01 -0500

    update script

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("hello from other github!");
+
+function foo() {
+  const bar = "baz";
+
+  if (bar === "foobar") {
+    console.log("foobar!");
+  } else {
+    console.log("baz!");
+  }
+}
+
+foo();
